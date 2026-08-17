7e645790b292486bc351ce60bfa1e7380fbf2557
Luís Fernando Bicalho <lfernandotexbicalho@hotmail.com>
2026-08-17 14:24:30 -0300

    Add IMC calculation function and user input

---
+++ main2.py
@@ -0,0 +1,8 @@
+def calcula_imc(peso, altura):
+  return peso/altura**2
+
+altura = input("Digite a sua altura:")
+peso = input("Digite o seu peso:")
+
+imc = calcula_imc(peso, altura)
+print(f"O seu IMC é {imc}")
